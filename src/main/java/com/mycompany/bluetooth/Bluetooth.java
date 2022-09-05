3f397951a8f928ccb6e92da52a1a4e07244cfe55
[jmbbenavides] <[1942395@clases.edu.sv]>
2022-09-05 21:08:29 +0000

    Primer commit

---
+++ src/main/java/com/mycompany/bluetooth/Bluetooth.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.bluetooth;
+
+/**
+ *
+ * @author jmbb
+ */
+public class Bluetooth {
+
+    public static void main(String[] args) {
+       boolean isBluetoothEnabled = false;
+       int filesSended = 3;
+       if (isBluetoothEnabled) {
+        fileSended++;
+            System.out.println(&quot;Archivo enviado&quot;);
+      } else {
+            System.out.println(&quot;El Bluetooth no está activado&quot;);
+}
+    }
+}
+
